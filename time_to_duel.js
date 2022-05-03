8d05fca6a1138df55eadc60190ff4c44c3a844c3
Jonathan <jonathankwon@yahoo.com>
2022-05-03 14:58:00 -0700

    Created instances of Unit and Effect cards and input logic

--- time_to_duel.js
+++ time_to_duel.js
@@ -1,12 +1,12 @@
 class Card {
-    constructer(name, cost) {
+    constructor(name, cost) {
         this.name = name;
         this.cost = cost;
     }
 }
 
 class Unit extends Card {
-    constructer(name, cost, power, res){
+    constructor(name, cost, power, res){
         super(name, cost);
         this.power = power;
         this.res = res;
@@ -18,7 +18,7 @@ class Unit extends Card {
 }
 
 class Effect extends Card {
-    constructer(name, cost, text, stat, magnitude) {
+    constructor(name, cost, text, stat, magnitude) {
         super(name, cost);
         this.text = text;
         this.stat = stat;
@@ -27,11 +27,35 @@ class Effect extends Card {
 
     play(target){
         if (target instanceof Unit) {
-            console.log("Raise the target's resilience by 2");
+            console.log(this.stat);
+            if (this.stat === "Power"){
+                target.power += this.magnitude
+            }
+            if (this.stat === "Resilience"){
+                target.res += this.magnitude
+            }
+            // console.log("The target is a unit.");
         }
         else {
             throw new Error("Target must be a unit!")
         }
-        target.res += 2;
     }
 }
+
+// Unit Objects
+const redNinja = new Unit('Red Belt Ninja', 3, 3, 4);
+const blackNinja = new Unit('Black Belt Ninja', 4, 5, 4)
+
+// Effect Objects
+const hardAlgo = new Effect("Hard Algorithm", 2, "increase target's resilience by 3", "Resilience", 3);
+const uPR = new Effect("Unhandled Promise Rejection", 1, "reduce target's resilience by 2","Resilience", -2);
+const pairProgramming = new Effect("Pair Programming", 3, "increase target's power by 2", "Power", 2);
+
+
+hardAlgo.play(redNinja);
+uPR.play(redNinja);
+pairProgramming.play(redNinja);
+redNinja.attack(blackNinja);
+
+console.log(redNinja);
+console.log(blackNinja);
